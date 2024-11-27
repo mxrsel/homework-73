58c66f1604dee9fd3bcd91bbdab1ee433b9ca913
mxrsel <marsel14082006@gmail.com>
2024-11-28 00:56:04 +0600

    added main page and params for tracking text from search line to page

--- 01/index.js
+++ 01/index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 
 const app = express();
+const port = 8000;
+
+app.get('/hello', (req, res) => {
+    return res.send('Hello page')
+})
+
+app.get('/hello/:randomText', (req, res) => {
+    return res.send(req.params.randomText);
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port: http://localhost:${port}`);
+})
